6083ed021da7c6b33af6abab6720df9c0401ade4
Lalitroca <67077073+Lalitroca@users.noreply.github.com>
2020-06-24 13:25:17 -0300

    Create template-book.php

---
+++ wp-content/themes/clubinho-do-livro/template-book.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Template Name: Book-Page layout
+ * Template Post Type: book, page
+ */
+
+get_header();
+?>
+
+<main id="site-content" role="main">
+
+	<?php
+
+	if ( have_posts() ) {
+
+		while ( have_posts() ) {
+			the_post();
+
+			get_template_part( 'template-parts/content', get_post_type() );
+		}
+	}
+
+	?>
+
+</main><!-- #site-content -->
+
+<button id="startRecord">Gravar</button>
+<button id="stopRecord>Parar</button>
+
+<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
+
+<?php get_footer(); ?>
+
+<script>  
+  var recordButton = document.getElementById('startRecord');
+  var stopButton = document.getElementById('stopRecord');
+  recordButton.addEventListener("click", startRecording);
+  stopButton.addEventListener("click", stopRecording);
+  debug = document.getElementById("debugText");
+  var recording = false;
+
+    
+  recordButton.disabled = false;
+  stopButton.disabled = true;
+  
+  var handleSuccess = function(stream) {
+
+    console.log("Sucess");
+
+    var context = new AudioContext();
+    var input = context.createMediaStreamSource(stream);
+    var processor = context.createScriptProcessor(1024,1,1);
+    
+    //input.connect(processor);
+    processor.connect(context.destination);
+    
+    const options = {
+      mimeType: "video/webm;codecs=vp9" //ESCOLHER FORMATO IDEAL
+    };
+    var recordedChunks = [];
+    
+    
+    recorder = new MediaRecorder(stream, options);
+    
+    recorder.onstart = function(e) {
+      console.log("Started");
+    }
+
+    recorder.ondataavailable = function(e) {
+      if (e.data.size > 0) {
+        recordedChunks.push(e.data);
+      }
+    };
+
+    recorder.onstop = function() {
+      debug.innerHTML = "STOPED";
+      recording = false;
+      //new Blob(recordedChunks);
+      //FAZER O QUE FOR NECESSÁRIO COM OS AUDIOS
+    };
+    
+    if(recording) {
+      console.log("Recording...");
+      recorder.start();
+    }
+  };
+  
+  function startRecording() {
+    recordButton.disabled = true;
+    stopButton.disabled = false;
+
+    recording = true;
+    
+    navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+        .then(handleSuccess)
+  }
+  
+  function stopRecording() {
+    stopButton.disabled = true;
+    recordButton.disabled = false;
+    console.log("Asked to stop");
+    
+    recorder.stop();
+  }
+</script>
